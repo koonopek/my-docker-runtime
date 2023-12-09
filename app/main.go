49009605909a0eb0e5d739d540a80f8d56ec2b7f
Michal Konopka <konopek68@gmail.com>
2023-12-09 15:53:52 +0100

    next

--- app/main.go
+++ app/main.go
@@ -31,7 +31,7 @@ func main() {
 
 	os.MkdirAll(path.Dir(newPath), 0666)
 
-	copyTo, err := os.OpenFile(newPath, os.O_CREATE|os.O_WRONLY, os.ModeAppend)
+	copyTo, err := os.OpenFile(newPath, os.O_CREATE|os.O_RDWR, os.ModeAppend)
 	if err != nil {
 		fmt.Printf("Failed to open %s, error %s", newPath, err.Error())
 		os.Exit(2)
