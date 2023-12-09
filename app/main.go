d698114129f0f0b3a11825f59a82faedf9d94f06
Michal Konopka <konopek68@gmail.com>
2023-12-09 19:40:52 +0100

    next

--- app/main.go
+++ app/main.go
@@ -6,6 +6,8 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"os/exec"
 	"path"
@@ -14,51 +16,17 @@ import (
 
 const JAIL_DIR = "jail"
 
-// Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
 	var err error
 
-	// Uncomment this block to pass the first stage!
 	command := os.Args[3]
 	userArgs := os.Args[4:len(os.Args)]
 
 	os.Mkdir(JAIL_DIR, 0777)
 
-	copyFrom, err := os.Open(command)
-	if err != nil {
-		panic(fmt.Sprintf("Failed to open %s, error %s", command, err.Error()))
-	}
-	defer copyFrom.Close()
+	copyFileToJail(command)
 
-	newPath := path.Join(JAIL_DIR, command)
-
-	os.MkdirAll(path.Dir(newPath), 0777)
-
-	copyTo, err := os.OpenFile(newPath, os.O_CREATE|os.O_RDWR, 0777)
-	if err != nil {
-		panic(fmt.Sprintf("Failed to open %s, error %s", newPath, err.Error()))
-	}
-
-	_, err = io.Copy(copyTo, copyFrom)
-	if err != nil {
-		panic(fmt.Sprintf("Failed to copy file %s, error %s", newPath, err.Error()))
-	}
-
-	copyTo.Close()
-
-	args := append([]string{JAIL_DIR, command}, userArgs...)
-
-	cmd := exec.Command("chroot", args...)
-
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: uintptr(syscall.CLONE_NEWPID | CLONE_NEWNS),
-	}
-
-	cmd.Stdout = os.Stdout
-	cmd.Stdin = os.Stdin
-	cmd.Stderr = os.Stderr
-
-	err = cmd.Run()
+	err = runInContainer(command, userArgs, err)
 
 	switch err := err.(type) {
 	case nil:
@@ -70,4 +38,61 @@ func main() {
 		os.Exit(124)
 	}
 
+}
+
+func runInContainer(command string, userArgs []string, err error) error {
+	args := append([]string{JAIL_DIR, command}, userArgs...)
+	cmd := exec.Command("chroot", args...)
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: uintptr(syscall.CLONE_NEWPID),
+	}
+
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+
+	return cmd.Run()
+}
+
+func copyFileToJail(toCopy string) {
+	copyFrom, err := os.Open(toCopy)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to open %s, error %s", toCopy, err.Error()))
+	}
+	defer copyFrom.Close()
+
+	copyToPath := path.Join(JAIL_DIR, toCopy)
+
+	os.MkdirAll(path.Dir(copyToPath), 0777)
+
+	copyTo, err := os.OpenFile(copyToPath, os.O_CREATE|os.O_RDWR, 0777)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to open %s, error %s", copyToPath, err.Error()))
+	}
+
+	_, err = io.Copy(copyTo, copyFrom)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to copy file %s, error %s", copyToPath, err.Error()))
+	}
+
+	copyTo.Close()
+}
+
+func fetchImage(imageName *string) error {
+	response, err := http.Get(fmt.Sprintf("https://auth.docker.io/token?service=registry.docker.io&scope=repository:library/%s:pull", imageName))
+
+	if err != nil || response.StatusCode != 200 {
+		return fmt.Errorf("Failed to fetch authorization token")
+	}
+
+	body, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		return fmt.Errorf("Failed to read body")
+	}
+
+	fmt.Sprintf(string(body))
+
+	return nil
 }
