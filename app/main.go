5f3c72a8f8b6ff7f520d54db510c47f6272b9bb8
Michal Konopka <konopek68@gmail.com>
2023-12-09 15:25:26 +0100

    next

--- app/main.go
+++ app/main.go
@@ -6,12 +6,18 @@ import (
 	"os/exec"
 )
 
+const JAIL_DIR = "jail"
+
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
 
 	// Uncomment this block to pass the first stage!
 	command := os.Args[3]
-	args := os.Args[4:len(os.Args)]
+	userArgs := os.Args[4:len(os.Args)]
+
+	os.Mkdir(JAIL_DIR, 0666)
+
+	args := append([]string{"chroot", "jail"}, userArgs...)
 
 	cmd := exec.Command(command, args...)
 
